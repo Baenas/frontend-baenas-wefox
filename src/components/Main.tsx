262090433e5ad3f6e6dcc105cff4af7825d09656
Baenas <57912626+Baenas@users.noreply.github.com>
2021-07-09 17:15:49 +0200

    new update : data
    the title value from the api is rendered

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import axios from 'axios';
+import { stat } from 'fs';
 type MyProps = {
   // using `interface` is also ok
   message: string;
 };
-type MyState = {
-  posts: object; // like this
+
+class Test {
+  title: string
+  constructor() {
+    this.title = "";
+  }
+ 
+}
+
+interface MyState  {
+  posts: Array<{title: string}>; // like this
 };
+
 class Main extends React.Component<MyProps, MyState> {
   state: MyState = {
     // optional second annotation for better type inference
@@ -14,13 +25,29 @@ class Main extends React.Component<MyProps, MyState> {
   };
   componentDidMount(){
     axios.get("http://localhost:3000/api/v1/posts").then((data) => {
-    this.setState({posts: data.data})
+    this.setState({posts : data.data })
+    
     })
-  }
+    }
+
+   display = () => {
+    console.log(this.state.posts)
+
+    
+   return  this.state.posts.map((contenido,i ) => {
+      return(
+        <div key={i}>
+          {contenido.title}
+        
+        </div>
+      )
+    })
+}
   render() {
+  
     return (
       <div>
-    a
+        {this.display()}
       </div>
     );
   }
